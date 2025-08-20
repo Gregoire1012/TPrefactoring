a27fbcb2c1c1d8e9f59c2f0f92fe6e1742b1dc47
Gregoire1012 <gregoirejeanlouis12@gmail.com>
2025-08-20 11:20:36 +0200

    question 12

--- TPrefactoring/src/iut/bad/Humain.java
+++ TPrefactoring/src/iut/bad/Humain.java
@@ -1,20 +1,26 @@
 package iut.bad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Humain implements Consommation {
     private String nom;
     private String prenom;
     private int age;
+    private List<Humain> amis;
 
     public Humain() {
         this.nom = "";
         this.prenom = "";
         this.age = 0;
+        this.amis = new ArrayList<>();
     }
 
     public Humain(String nom, String prenom, int age) {
         this.nom = nom;
         this.prenom = prenom;
         this.age = age;
+        this.amis = new ArrayList<>();
     }
 
     public String getNom() {
@@ -58,5 +64,20 @@ public class Humain implements Consommation {
     @Override
     public void boire() {
         System.out.println(getNom() + " est en train de boire.");
+    }
+
+    public void ami(Humain h) {
+        if (!amis.contains(h)) {
+            amis.add(h);
+            System.out.println(this.nom + " est maintenant ami avec " + h.getNom());
+        }
+    }
+
+    public void listerAmis() {
+        System.out.print(this.nom + " a pour amis : ");
+        for (Humain h : amis) {
+            System.out.print(h.getNom() + " ");
+        }
+        System.out.println();
     }
 }
