3981dd8acf4903fb477fb5c25bcbb132a5c43079
Gregoire1012 <gregoirejeanlouis12@gmail.com>
2025-08-20 11:27:44 +0200

    question 13

--- TPrefactoring/src/iut/bad/Femme.java
+++ TPrefactoring/src/iut/bad/Femme.java
@@ -8,4 +8,23 @@ public class Femme extends Humain {
     public Femme(String nom, String prenom, int age) {
         super(nom, prenom, age);
     }
+
+    @Override
+    public String toString() {
+        return "Femme " + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Homme h = new Homme("Rakoto", "Jean", 25);
+        Femme f = new Femme("Rabe", "Marie", 22);
+
+        h.ami(f);
+        f.ami(h);
+
+        h.details();
+        f.details();
+
+        h.listerAmis();
+        f.listerAmis();
+    }
 }
