358eedbee0fc028532d8d3ca9a575b4fca65a60f
Gregoire1012 <gregoirejeanlouis12@gmail.com>
2025-08-20 09:00:47 +0200

    question 4

---
+++ TPrefactoring/src/iut/bad/Femme.java
@@ -0,0 +1,52 @@
+package iut.bad;
+
+public class Femme {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Femme() {
+        this.nom = "";
+        this.prenom = "";
+        this.age = 0;
+    }
+
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Femme{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
